d5a372749dc7bd8abae026e536375cce24243e99
tjquillan <tjquillan@gmail.com>
2021-05-09 10:35:48 -0700

    Optimize internal looping

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,29 @@ import { ErrorMapper } from "utils/ErrorMapper";
 import { Harvester } from "roles/harvester";
 import { Upgrader } from "roles/upgrader";
 
-// When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
-// This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  // Automatically delete memory of missing creeps
+  const creeps: Map<string, Creep[]> = new Map<string, Creep[]>();
   for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
+    if (name in Game.creeps) {
+      const creep = Game.creeps[name];
+      if (!creeps.has(creep.memory.role)) {
+        creeps.set(creep.memory.role, []);
+      }
+      creeps.get(creep.memory.role)?.push(creep);
+    } else {
+      // Delete memory of missing creeps
       delete Memory.creeps[name];
     }
   }
 
-  const harvesters: Creep[] = Object.values(Game.creeps).filter((creep) => creep.memory.role === "harvester");
-  const upgraders: Creep[] = Object.values(Game.creeps).filter((creep) => creep.memory.role === "upgrader");
-
-  if (harvesters.length < 2) {
+  let role = creeps.get("harvester");
+  if (!role || role.length < 2) {
     const newName = `Harvester ${Game.time}`;
     Game.spawns.Spawn1.spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: "harvester", working: false } });
   }
 
-  if (upgraders.length < 1) {
+  role = creeps.get("upgrader");
+  if (!role || role.length < 3) {
     const newName = `Upgrader ${Game.time}`;
     Game.spawns.Spawn1.spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: "upgrader", working: false } });
   }
